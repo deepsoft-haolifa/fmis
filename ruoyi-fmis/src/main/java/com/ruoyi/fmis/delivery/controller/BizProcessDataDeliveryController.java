2a9c5e587a34a746b2471dbdda0c4cf8b0f2ca63
李可心 <beidouchain004@outlook.com>
2020-05-07 16:24:15 +0800

    发货管理增删改查功能实现

--- ruoyi-fmis/src/main/java/com/ruoyi/fmis/delivery/controller/BizProcessDataDeliveryController.java
+++ ruoyi-fmis/src/main/java/com/ruoyi/fmis/delivery/controller/BizProcessDataDeliveryController.java
@@ -1,11 +1,23 @@
 package com.ruoyi.fmis.delivery.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import com.ruoyi.fmis.common.BizConstants;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.fmis.child.domain.BizProcessChild;
+import com.ruoyi.fmis.child.service.IBizProcessChildService;
 import com.ruoyi.fmis.common.CommonUtils;
+import com.ruoyi.fmis.customer.domain.BizCustomer;
+import com.ruoyi.fmis.customer.service.IBizCustomerService;
+import com.ruoyi.fmis.data.domain.BizProcessData;
+import com.ruoyi.fmis.data.service.IBizProcessDataService;
 import com.ruoyi.fmis.define.service.IBizProcessDefineService;
+import com.ruoyi.fmis.product.service.IBizProductService;
 import com.ruoyi.system.domain.SysRole;
 import com.ruoyi.system.service.ISysRoleService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -13,22 +25,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.enums.BusinessType;
-import com.ruoyi.fmis.data.domain.BizProcessData;
-import com.ruoyi.fmis.data.service.IBizProcessDataService;
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.common.core.page.TableDataInfo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 /**
- * 合同管理Controller
+ * 发货管理Controller
  *
  * @author frank
  * @date 2020-05-05
@@ -47,6 +50,15 @@ public class BizProcessDataDeliveryController extends BaseController {
     @Autowired
     private IBizProcessDefineService bizProcessDefineService;
 
+    @Autowired
+    private IBizProcessChildService bizProcessChildService;
+
+    @Autowired
+    private IBizProductService bizProductService;
+
+    @Autowired
+    private IBizCustomerService bizCustomerService;
+
     @RequiresPermissions("fmis:delivery:view")
     @GetMapping()
     public String data() {
@@ -54,7 +66,7 @@ public class BizProcessDataDeliveryController extends BaseController {
     }
 
     /**
-     * 查询合同管理列表
+     * 查询发货管理列表
      */
     @RequiresPermissions("fmis:delivery:list")
     @PostMapping("/list")
@@ -71,6 +83,13 @@ public class BizProcessDataDeliveryController extends BaseController {
         if (!CollectionUtils.isEmpty(flowMap)) {
             //计算流程描述
             for (BizProcessData data : list) {
+                //获取客户名称
+                String customerId = data.getString2();
+                if (StringUtils.isNotEmpty(customerId)) {
+                    BizCustomer customer = bizCustomerService.selectBizCustomerById(Long.parseLong(customerId));
+                    data.setBizCustomer(customer);
+                    data.setCustomerName(customer.getName());
+                }
                 String flowStatus = data.getFlowStatus();
                 //结束标识
                 String normalFlag = data.getNormalFlag();
@@ -138,7 +157,7 @@ public class BizProcessDataDeliveryController extends BaseController {
         return prefix + "/viewExamineHistory";
     }
     /**
-     * 导出合同管理列表
+     * 导出发货管理列表
      */
     @RequiresPermissions("fmis:delivery:export")
     @PostMapping("/export")
@@ -150,7 +169,7 @@ public class BizProcessDataDeliveryController extends BaseController {
     }
 
     /**
-     * 新增合同管理
+     * 新增发货管理
      */
     @GetMapping("/add")
     public String add() {
@@ -158,10 +177,10 @@ public class BizProcessDataDeliveryController extends BaseController {
     }
 
     /**
-     * 新增保存合同管理
+     * 新增保存发货管理
      */
     @RequiresPermissions("fmis:delivery:add")
-    @Log(title = "合同管理", businessType = BusinessType.INSERT)
+    @Log(title = "发货管理", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
     public AjaxResult addSave(BizProcessData bizProcessData) {
@@ -174,33 +193,98 @@ public class BizProcessDataDeliveryController extends BaseController {
                 bizProcessData.setNormalFlag(key);
             }
         }
-
-        return toAjax(bizProcessDataService.insertBizProcessData(bizProcessData));
+        String productArrayStr = bizProcessData.getProductParmters();
+        int insertReturn = bizProcessDataService.insertBizProcessData(bizProcessData);
+        Long dataId = bizProcessData.getDataId();
+        if (StringUtils.isNotEmpty(productArrayStr)) {
+            JSONArray productArray = JSONArray.parseArray(productArrayStr);
+            for (int i = 0; i < productArray.size(); i++) {
+                JSONObject json = productArray.getJSONObject(i);
+                BizProcessChild bizProcessChild = JSONObject.parseObject(json.toJSONString(), BizProcessChild.class);
+                if (StringUtils.isNotEmpty(bizProcessChild.getString1())) {
+                    bizProcessChild.setDataId(dataId);
+                    bizProcessChildService.insertBizProcessChild(bizProcessChild);
+                }
+            }
+        }
+        return toAjax(insertReturn);
     }
 
     /**
-     * 修改合同管理
+     * 修改发货管理
      */
     @GetMapping("/edit/{dataId}")
     public String edit(@PathVariable("dataId") Long dataId, ModelMap mmap) {
         BizProcessData bizProcessData = bizProcessDataService.selectBizProcessDataById(dataId);
+
+        BizProcessChild queryBizProcessChild = new BizProcessChild();
+        queryBizProcessChild.setDataId(bizProcessData.getDataId());
+        List<BizProcessChild> bizProcessChildList = bizProcessChildService.selectBizProcessChildList(queryBizProcessChild);
+        String productNames = "";
+        String productIds = "";
+        if (!CollectionUtils.isEmpty(bizProcessChildList)) {
+            for (BizProcessChild bizProcessChild : bizProcessChildList) {
+                String productId = bizProcessChild.getString1();
+                BizProcessData bizProduct = bizProcessDataService.selectBizProcessDataById(Long.parseLong(productId));
+                productNames += bizProduct.getString1() + ",";
+                productIds += bizProduct.getDataId() + ",";
+                bizProcessChild.setBizProcessData(bizProduct);
+            }
+            bizProcessData.setBizProcessChildList(bizProcessChildList);
+        }
+        String customerId = bizProcessData.getString2();
+        if (StringUtils.isNotEmpty(customerId)) {
+            BizCustomer customer = bizCustomerService.selectBizCustomerById(Long.parseLong(customerId));
+            bizProcessData.setBizCustomer(customer);
+            bizProcessData.setCustomerName(customer.getName());
+        }
+
+        mmap.put("contractNames", productNames);
+        mmap.put("contractIds", productIds);
+
         mmap.put("bizProcessData", bizProcessData);
         return prefix + "/edit";
     }
 
     /**
-     * 修改保存合同管理
+     * 修改保存发货管理
      */
     @RequiresPermissions("fmis:delivery:edit")
-    @Log(title = "合同管理", businessType = BusinessType.UPDATE)
+    @Log(title = "发货管理", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
     public AjaxResult editSave(BizProcessData bizProcessData) {
-        return toAjax(bizProcessDataService.updateBizProcessData(bizProcessData));
+        String productArrayStr = bizProcessData.getProductParmters();
+        int updateReturn = bizProcessDataService.updateBizProcessData(bizProcessData);
+
+        Long dataId = bizProcessData.getDataId();
+
+        BizProcessChild removeBizProcuessChild = new BizProcessChild();
+        removeBizProcuessChild.setDataId(dataId);
+        List<BizProcessChild> removeBizProcessChildList = bizProcessChildService.selectBizProcessChildList(removeBizProcuessChild);
+        if (!CollectionUtils.isEmpty(removeBizProcessChildList)) {
+            for (BizProcessChild bizProcessChild : removeBizProcessChildList) {
+                bizProcessChildService.deleteBizProcessChildById(bizProcessChild.getChildId());
+            }
+        }
+
+        if (StringUtils.isNotEmpty(productArrayStr)) {
+            JSONArray productArray = JSONArray.parseArray(productArrayStr);
+            for (int i = 0; i < productArray.size(); i++) {
+                JSONObject json = productArray.getJSONObject(i);
+                BizProcessChild bizProcessChild = JSONObject.parseObject(json.toJSONString(), BizProcessChild.class);
+                if (StringUtils.isNotEmpty(bizProcessChild.getString1())) {
+                    bizProcessChild.setDataId(dataId);
+                    bizProcessChildService.insertBizProcessChild(bizProcessChild);
+                }
+
+            }
+        }
+        return toAjax(updateReturn);
     }
 
     /**
-     * 删除合同管理
+     * 删除发货管理
      */
     @RequiresPermissions("fmis:delivery:remove")
     @Log(title = "合同管理", businessType = BusinessType.DELETE)
@@ -216,5 +300,21 @@ public class BizProcessDataDeliveryController extends BaseController {
         String dataId = getRequest().getParameter("dataId");
         BizProcessData bizQuotation = bizProcessDataService.selectBizProcessDataById(Long.parseLong(dataId));
         return toAjax(bizProcessDataService.subReportBizQuotation(bizQuotation));
+    }
+
+    /**
+     * 选择客户
+     */
+    @GetMapping("/selectCustomer")
+    public String selectCustomer() {
+        return prefix + "/selectCustomer";
+    }
+
+    /**
+     * 选择合同
+     */
+    @GetMapping("/selectContract")
+    public String selectContract(ModelMap mmap) {
+        return prefix + "/selectContract";
     }
 }
